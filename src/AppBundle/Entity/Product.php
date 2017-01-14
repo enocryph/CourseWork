04d686161336927905685469d8ee29c67a341f6b
Vadim <enocryph@yandex.ru>
2017-01-14 03:20:23 +0300

    added Product entity and repository

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateOfCreation", type="datetime")
+     */
+    private $dateOfCreation;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateOfLastUpdate", type="datetime")
+     */
+    private $dateOfLastUpdate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isActive", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uniqueIdentifier", type="string", length=255, unique=true)
+     */
+    private $uniqueIdentifier;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set dateOfCreation
+     *
+     * @param \DateTime $dateOfCreation
+     *
+     * @return Product
+     */
+    public function setDateOfCreation($dateOfCreation)
+    {
+        $this->dateOfCreation = $dateOfCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateOfCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateOfCreation()
+    {
+        return $this->dateOfCreation;
+    }
+
+    /**
+     * Set dateOfLastUpdate
+     *
+     * @param \DateTime $dateOfLastUpdate
+     *
+     * @return Product
+     */
+    public function setDateOfLastUpdate($dateOfLastUpdate)
+    {
+        $this->dateOfLastUpdate = $dateOfLastUpdate;
+
+        return $this;
+    }
+
+    /**
+     * Get dateOfLastUpdate
+     *
+     * @return \DateTime
+     */
+    public function getDateOfLastUpdate()
+    {
+        return $this->dateOfLastUpdate;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return Product
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set uniqueIdentifier
+     *
+     * @param string $uniqueIdentifier
+     *
+     * @return Product
+     */
+    public function setUniqueIdentifier($uniqueIdentifier)
+    {
+        $this->uniqueIdentifier = $uniqueIdentifier;
+
+        return $this;
+    }
+
+    /**
+     * Get uniqueIdentifier
+     *
+     * @return string
+     */
+    public function getUniqueIdentifier()
+    {
+        return $this->uniqueIdentifier;
+    }
+}
+
