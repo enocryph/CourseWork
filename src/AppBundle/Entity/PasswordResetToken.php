29689ee63488c56a1e0adb038734a80131778e73
Sasha <sashka.k777@gmail.com>
2017-01-14 18:37:17 +0300

    create resettoken entity

---
+++ src/AppBundle/Entity/PasswordResetToken.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qwerty
+ * Date: 14.01.17
+ * Time: 18:35
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * PasswordResetToken
+ *
+ * @ORM\Table(name="password_reset_token")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\PasswordResetTokenRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class PasswordResetToken
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=255, unique=true)
+     */
+    private $token;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return PasswordResetToken
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+        return $this;
+    }
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return PasswordResetToken
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+        return $this;
+    }
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return PasswordResetToken
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+    /**
+     * Now we tell doctrine that before we persist or update we call the updatedTimestamps() function.
+     *
+     * @ORM\PrePersist
+     */
+    public function updatedTimestamps()
+    {
+        $this->setCreated(new \DateTime());
+    }
+}
