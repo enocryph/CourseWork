f8d629368fd538e0852de8c38db7135dd52c73eb
Sasha <sashka.k777@gmail.com>
2017-01-15 04:07:26 +0300

    some kind of shit
    i dont know what i done

--- src/AppBundle/Form/CategoryType.php
+++ src/AppBundle/Form/CategoryType.php
@@ -13,7 +13,7 @@ class CategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('parent')        ;
+        $builder->add('title')->add('parent');
     }
     
     /**
