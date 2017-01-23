f711ddc01cbf4ce9b2a2bf61543520720f667923
Sasha <sashka.k777@gmail.com>
2017-01-23 04:24:25 +0300

    add routing and access control

---
+++ src/AppBundle/Security/AccessDeniedHandler.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qwerty
+ * Date: 23.01.17
+ * Time: 4:07
+ */
+
+namespace AppBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return new RedirectResponse('http://vk.com');
+    }
+}
